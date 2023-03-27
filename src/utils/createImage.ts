d61690bf451a18845c595f01b08612c4574baa1b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-03-28 04:34:19 +0700

    feat: menambahkan handlebars dan memperbaiki ukuran gambar

--- src/utils/createImage.ts
+++ src/utils/createImage.ts
@@ -1,62 +1,22 @@
+import Handlebars from "handlebars";
 import puppeteer from "puppeteer";
-import QRCode from "qrcode";
 import fs from "fs/promises";
+import QRCode from "qrcode";
+import normalFs from "fs";
+import path from "path";
 
 interface IParams {
   nama: string;
   qrId: string;
 }
 
-const genHtml = ({
-  nama,
-  qrId,
-  qrString,
-}: IParams & { qrString: string }) => `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Cutive+Mono&family=Rubik:wght@500&display=swap" rel="stylesheet" />
-
-    <title>QR hak pilih ${nama} | ${qrId}</title>
-</head>
-<body>
-    <main>
-        <img src="${qrString}" />
-
-        <h1>${nama}</h1>
-
-        <pre>${qrId}</pre>
-    </main>
-
-    <style>
-        main {
-            display: flex;
-            flex-direction: column;
-            width: 100vw;
-            height: 100vh;
-        }
-
-        h1 {
-            text-align: center;
-            font-size: 6em;
-            width: 90%;
-            align-self: center;
-            font-family: 'Rubik', sans-serif;
-        }
-    
-        pre {
-            text-align: center;
-            font-size: 4.5em;
-            font-family: 'VT323', monospace;
-        }
-    </style>
-</body>
-</html>`;
+const templateString = normalFs.readFileSync(
+  path.join(path.resolve(), "src/layouts/Gambar.handlebars"),
+  "utf8"
+);
+const template = Handlebars.compile<IParams & { qrString: string }>(
+  templateString
+);
 
 export const createImage = async ({
   fileName,
@@ -70,9 +30,11 @@ export const createImage = async ({
     const qrString = await QRCode.toDataURL(qrId, { width: 950 });
 
     const page = await browser.newPage();
-    await page.setViewport({ width: 950, height: 1585 });
+    await page.setViewport({ width: 950, height: 1655 });
 
-    await page.setContent(genHtml({ qrId, qrString, nama }));
+    await page.setContent(template({ qrId, qrString, nama }), {
+      waitUntil: "networkidle0",
+    });
 
     const ss = await page.screenshot();
 
